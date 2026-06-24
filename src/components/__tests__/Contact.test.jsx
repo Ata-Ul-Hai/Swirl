8d9f776c3ba0f2ae9135bfc6626076101d3e8aba
Ata-Ul-Hai <ataulhai2007@gmail.com>
2026-06-24 17:02:01 +0530

    wrote basic tests for contact page (learning pov)

---
+++ src/components/__tests__/Contact.test.jsx
@@ -0,0 +1,51 @@
+import { render, screen } from "@testing-library/react"
+import Contact from "../Contact"
+import { describe, expect, it, test } from "vitest"
+
+describe("Tests for Contact Page", () => {
+    test('Should check if heading is being rendered',() => {
+        render(<Contact />)
+
+        const heading = screen.getByRole('heading')
+
+        expect(heading).toBeInTheDocument()
+    })
+
+    test('sould check submit button', () => {
+        render(<Contact />)
+
+        const button = screen.getByRole('button')
+
+        // Assertion
+        expect(button).toBeInTheDocument()
+    })
+
+    test("Should check if there is an element with the text Submit", () => {
+        render(<Contact />)
+
+        const submit = screen.getByText('Submit')
+
+        expect(submit).toBeInTheDocument()
+    })
+
+    it('Should check input by it\'s placeholder', ()=>{
+        render(<Contact />)
+
+        const placeholder = screen.getByPlaceholderText('name')
+
+        expect(placeholder).toBeInTheDocument()
+    })
+
+    it('Should check there are 2 input boxes', ()=>{
+        render(<Contact />)
+
+        // Query
+        const len = screen.getAllByRole('textbox')
+
+        expect(len.length).toBe(2)
+    })
+
+})
+
+
+
